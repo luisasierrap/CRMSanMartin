fdbec8505d644f3edcaa828dfae49bab57f830aa
luisasierrap <luisasierrap97@gmail.com>
2019-03-10 18:38:12 -0500

    CRMSanMartin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import  * as M from "materialize-css";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CRMSanMartin';
+
+  ngOnInit(): void {
+    let elems = document.querySelectorAll('.carousel');
+    let instances = M.Carousel.init(elems, {});
+    var navbar = document.querySelectorAll('.sidenav');
+    var navBarInstance = M.Sidenav.init(navbar, {});
+  }
+
 }
